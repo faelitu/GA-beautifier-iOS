d9fe80ad39472123442b50b41736270e0d8aab96
Rafael Machado <rafael.machado@dp6.com.br>
2021-11-05 18:13:19 -0300

    entire code refactored to use dictionary

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # GA-beautifier-iOS
 
-This code makes the logs provided by Fiddler proxy of the Google Analytics events coming from iOS more readable.
+This code makes the logs provided by Fiddler proxy of the Google Analytics events coming from iOS more readable, by converting it to JSON.
 
 To run it, create a file name as "in.txt" with the decrypted logs and in a terminal run the "beautifier.py" file:
 
 `python3 beautifier.py`
 
-The result will be saved in the file "out.txt".
+The result will be saved in the file "out.json".
 
 Enjoy ;D
--- beautifier.py
+++ beautifier.py
@@ -1,85 +1,155 @@
 import urllib.parse
+import json
+import re
 
-params_file = open('parameters.txt', 'r')
 input = open('in.txt', 'r')
-output = open('out.txt', 'a')
 
-for line_in in input:
-    line_in = urllib.parse.unquote(line_in)
-    hits = [hit + '@\n\n\n' for hit in line_in.split('@')]
-    for i in range(0, len(hits)):
-        hits[i] = ['=' + par + '\n' for par in hits[i].split('&')]
+params_dict = {
+    'v':            'Protocol Version',
+    'tid':          'Web Property ID',
+    'aip':          'Anonymize IP',
+    'npa':          'Disabling Advertising Personalization',
+    'ds':           'Data Source',
+    'qt':           'Queue Time',
+    'z':            'Cache Buster',
+    'cid':          'Client ID',
+    'uid':          'User ID',
+    'sc':           'Session Control',
+    'uip':          'User IP',
+    'ua':           'User Agent',
+    'geoid':        'User Geographical Location',
+    'dr':           'Document Referrer',
+    'cn':           'Campaign Name',
+    'cs':           'Campaign Source',
+    'cm':           'Campaign Medium',
+    'ck':           'Campaign Keyword',
+    'cc':           'Campaign Content',
+    'ci':           'Campaign ID',
+    'gclid':        'Google Ads ID',
+    'dclid':        'Google Display Ads ID',
+    'sr':           'Screen Resolution',
+    'vp':           'Viewport Size',
+    'de':           'Document Encoding',
+    'sd':           'Screen Color Depth',
+    'ul':           'User Language',
+    'je':           'Is Java Enabled',
+    'fl':           'Flash Version',
+    't':            'Hit Type',
+    'ni':           'Is Non-Interactive Hit',
+    'dl':           'Document Location URL',
+    'dh':           'Document Host Name',
+    'dp':           'Document Path',
+    'dt':           'Document Title',
+    'cd':           'Screen Name',
+    'cg0':          'Content Group 0',
+    'linkid':       'Link ID',
+    'an':           'Application Name',
+    'aid':          'Application ID',
+    'av':           'Application Version',
+    'aiid':         'Application Installer ID',
+    'ec':           'Event Category',
+    'ea':           'Event Action',
+    'el':           'Event Label',
+    'ev':           'Event Value',
+    'ti':           'Transaction ID',
+    'ta':           'Transaction Affiliation',
+    'tr':           'Transaction Revenue',
+    'ts':           'Transaction Shipping',
+    'tt':           'Transaction Tax',
+    'in':           'Item Name',
+    'ip':           'Item Price',
+    'iq':           'Items Quantity',
+    'ic':           'Item Code',
+    'iv':           'Item Category',
+    'pr0id':        'Product 0 SKU',
+    'pr0nm':        'Product 0 Name',
+    'pr0br':        'Product 0 Brand',
+    'pr0ca':        'Product 0 Category',
+    'pr0va':        'Product 0 Variant',
+    'pr0pr':        'Product 0 Price',
+    'pr0qt':        'Product 0 Quantity',
+    'pr0cc':        'Product 0 Coupon Code',
+    'pr0ps':        'Product 0 Position',
+    'pr0cd1':       'Product 0 Custom Dimension 1',
+    'pr0cm1':       'Product 0 Custom Metric 1',
+    'pa':           'Product Action',
+    'tcc':          'Transaction Coupon Code',
+    'pal':          'Product Action List',
+    'cos':          'Checkout Step',
+    'col':          'Checkout Step Option',
+    'il0nm':        'Product Impression List 0 Name',
+    'il0pi1id':     'Product 1 Impression List 0 SKU',
+    'il0pi1nm':     'Product 1 Impression List 0 Name',
+    'il0pi1br':     'Product 1 Impression List 0 Brand',
+    'il0pi1ca':     'Product 1 Impression List 0 Category',
+    'il0pi1va':     'Product 1 Impression List 0 Variant',
+    'il0pi1ps':     'Product 1 Impression List 0 Position',
+    'il0pi1pr':     'Product 1 Impression List 0 Price',
+    'il0pi1cd2':    'Product 1 Impression List 0 Custom Dimension 2',
+    'il0pi1cm2':    'Product 1 Impression List 0 Custom Metric 2',
+    'promo0id':     'Promotion 0 ID',
+    'promo0nm':     'Promotion 0 Name',
+    'promo0cr':     'Promotion 0 Creative',
+    'promo0ps':     'Promotion 0 Position',
+    'promoa':       'Promotion Action',
+    'cu':           'Currency Code',
+    'sn':           'Social Network',
+    'sa':           'Social Action',
+    'st':           'Social Action Target',
+    'utc':          'User Timing Category',
+    'utv':          'User Timing Variable Name',
+    'utt':          'User Timing Time',
+    'utl':          'User Timing Label',
+    'plt':          'Page Load Time',
+    'dns':          'DNS Time',
+    'pdt':          'Page Download Time',
+    'rrt':          'Redirect Response Time',
+    'tcp':          'TCP Connect Time',
+    'srt':          'Server Response Time',
+    'dit':          'DOM Interactive Time',
+    'clt':          'Content Load Time',
+    'exd':          'Exception Description',
+    'exf':          'Is Exception Fatal',
+    'cd0':          'Custom Dimension 0',
+    'cm0':          'Custom Metric 0'
+}
 
-    params = []
-    for param in params_file:
-        [enc, dec] = param.split('=')
-        enc = enc + '='
-        dec = dec.replace('\n', '') + ' = '
-        params.append([enc, dec])
+batch = []
+for hit in input:
+    hit = urllib.parse.unquote(hit)
+    hit = hit.replace('%@\n', '')
+    pars = hit.split('&')
+    hit_obj = {}
+    for par in pars:
+        [key_ga, value] = par.split('=')
+        if not bool(re.search(r'\d', key_ga)):
+            key_readable = params_dict.get(key_ga)
+            if key_readable != None:
+                hit_obj.update({key_readable: value})
+            else:
+                hit_obj.update({key_ga: value})
+        else:
+            chars = [char for char in key_ga]
+            numbers = [number for number in chars if number.isnumeric()]
+            nums_dict = [str(num) for num in range(0, len(numbers))]
+            chars_dict = []
+            i = 0
+            for char in chars:
+                if not char.isnumeric():
+                    chars_dict.append(char)
+                else:
+                    chars_dict.append(nums_dict[i])
+                    i = i + 1
+            key_dict = ''.join(chars_dict)
+            key_readable = params_dict.get(key_dict)
+            for i in range(0, len(numbers)):
+                key_readable = key_readable.replace(nums_dict[i], numbers[i])
+            hit_obj.update({key_readable: value})
 
-    for param in params:
-        [enc, dec] = param
-        for i in range(0, len(hits)):
-            for j in range(0, len(hits[i])):
-                if enc.find('<number>') == -1:
-                    hits[i][j] = hits[i][j].replace('='+enc, dec)
+            
+    batch.append(hit_obj)
 
-                elif hits[i][j].startswith('='+enc.split('<number>')[0]) \
-                        and hits[i][j][len(enc.split('<number>')[0]) + 1].isnumeric() \
-                        and enc.find('<number2>') == -1:
-                    enc_split = enc.split('<number>')
-                    number = hits[i][j].replace('='+enc_split[0], '')[0]
-                    if hits[i][j].replace('='+enc_split[0], '')[1].isnumeric():
-                        number = number + hits[i][j].replace('='+enc_split[0], '')[1]
-                        if hits[i][j].replace('='+enc_split[0], '')[2].isnumeric():
-                            number = number + hits[i][j].replace('='+enc_split[0], '')[2]
-                    hits[i][j] = hits[i][j].replace('='+(enc.replace('<number>', number)), dec.replace('<number>', number))
-                
-                elif hits[i][j].startswith('='+enc.split('<number>')[0]) \
-                        and hits[i][j][len(enc.split('<number>')[0]) + 1].isnumeric() \
-                        and enc.find('<number2>') != -1 \
-                        and ''.join([i for i in hits[i][j] if not i.isdigit()])[3:5] == enc.split('<number>')[1][:2] \
-                        and enc.find('<number3>') == -1:
-                    enc_split = enc.split('<number>')
-                    number = hits[i][j].replace('='+enc_split[0], '')[0]
-                    if hits[i][j].replace('='+enc_split[0], '')[1].isnumeric():
-                        number = number + hits[i][j].replace('='+enc_split[0], '')[1]
-                        if hits[i][j].replace('='+enc_split[0], '')[2].isnumeric():
-                            number = number + hits[i][j].replace('='+enc_split[0], '')[2]
-                    number2 = hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0]), '')[0]
-                    if hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0]), '')[1].isnumeric():
-                        number2 = number2 + hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0]), '')[1]
-                        if hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0]), '')[2].isnumeric():
-                            number2 = number2 + hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0]), '')[2]
-                    hits[i][j] = hits[i][j].replace('='+(enc.replace('<number>', number).replace('<number2>', number2)), dec.replace('<number>', number).replace('<number2>', number2))
-
-                elif hits[i][j].startswith('='+enc.split('<number>')[0]) \
-                        and hits[i][j][len(enc.split('<number>')[0]) + 1].isnumeric() \
-                        and enc.find('<number2>') != -1 \
-                        and ''.join([i for i in hits[i][j] if not i.isdigit()])[3:5] == enc.split('<number>')[1][:2] \
-                        and enc.find('<number3>') != -1 \
-                        and ''.join([i for i in hits[i][j] if not i.isdigit()])[5:7] == enc.split('<number2>')[1][:2]:
-                    enc_split = enc.split('<number>')
-                    number = hits[i][j].replace('='+enc_split[0], '')[0]
-                    if hits[i][j].replace('='+enc_split[0], '')[1].isnumeric():
-                        number = number + hits[i][j].replace('='+enc_split[0], '')[1]
-                        if hits[i][j].replace('='+enc_split[0], '')[2].isnumeric():
-                            number = number + hits[i][j].replace('='+enc_split[0], '')[2]
-                    number2 = hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0]), '')[0]
-                    if hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0]), '')[1].isnumeric():
-                        number2 = number2 + hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0]), '')[1]
-                        if hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0]), '')[2].isnumeric():
-                            number2 = number2 + hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0]), '')[2]
-                    enc_split2 = enc.split('<number2>')
-                    number3 = hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0])+number2+(enc_split2[1].split('<number3>')[0]), '')[0]
-                    if hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0])+number2+(enc_split2[1].split('<number3>')[0]), '')[1].isnumeric():
-                        number3 = number3 + hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0])+number2+(enc_split2[1].split('<number3>')[0]), '')[1]
-                        if hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0])+number2+(enc_split2[1].split('<number3>')[0]), '')[2].isnumeric():
-                            number3 = number3 + hits[i][j].replace('='+enc_split[0]+number+(enc_split[1].split('<number2>')[0])+number2+(enc_split2[1].split('<number3>')[0]), '')[2]
-                    hits[i][j] = hits[i][j].replace('='+(enc.replace('<number>', number).replace('<number2>', number2).replace('<number3>', number3)), dec.replace('<number>', number).replace('<number2>', number2).replace('<number3>', number3))
-
-    for hit in hits:
-        for param in hit:
-            output.write(param)
+with open('out.json', 'w', encoding='utf-8') as f:
+    json.dump(batch, f, ensure_ascii=False, indent=4)
 
 print('DONE')
